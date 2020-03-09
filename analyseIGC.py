2e48cf9c75a99bea4253820a3f96c20a8b21173f
jpmacveigh <jpmacveigh.hotmail.fr>
2020-03-09 13:59:51 +0100

    commit initial

--- analyseIGC.py
+++ analyseIGC.py
@@ -3,4 +3,4 @@ path ="20140627 Saint-Auban 27 juin 2014 Discus 2b DP Copie de 46RXI0H1.igc"
 flight=FichierIGC(path)
 #flight.affiche()
 for i in range(250):
-    print(i,flight.getDateTime(i),flight.getDistance(i),flight.lignesB[i].gpsAlt,flight.getCapVitesse(i),flight.deltaCap(i),flight.getVz(i))
+    print(i,flight.getDeltaSecondes(i),flight.getDistance(i),flight.lignesB[i].gpsAlt,flight.getCapVitesse(i),flight.deltaCap(i),flight.getVz(i))
